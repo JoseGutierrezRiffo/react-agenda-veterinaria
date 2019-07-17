01d58c213d6e91a871542c1c371832dcf4314ca9
JoseGutierrezRiffo <icce.redes@gmail.com>
2019-07-17 16:09:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './componentes/Header';
+import AgregarCitas from './componentes/AgregarCitas';
+import ListaCitas from './componentes/ListaCitas';
+
 
 class App extends Component {
+
+  state = {
+    citas: []
+  }
+
+  crearCita = (nuevaCita) => {
+    const citas = [...this.state.citas, nuevaCita];
+    console.log(citas);
+
+    this.setState({
+      citas
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Header 
+          titulo={'Administrador de Pacientes de Veterinaria'}
+        />
+
+        <div className="row">
+          <div className="col-md-7">
+            <AgregarCitas 
+              crearCita={this.crearCita} 
+            />
+          </div>
+          <div className="col-md-5">
+            <ListaCitas 
+              citas={this.state.citas}
+            />
+          </div>
+        </div>
       </div>
     );
   }
